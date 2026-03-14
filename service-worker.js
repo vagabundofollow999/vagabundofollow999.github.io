d9dc1d56828cab1a84bebe6d89fa526da46c9df9
vagabundofollow999 <vagabundofollow999@gmail.com>
2026-03-14 11:29:37 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME="tigosports-v1";
+
+const urlsToCache=[
+"./",
+"./app.html"
+];
+
+self.addEventListener("install",event=>{
+
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache=>{
+return cache.addAll(urlsToCache);
+})
+);
+
+});
+
+self.addEventListener("fetch",event=>{
+
+event.respondWith(
+caches.match(event.request)
+.then(response=>{
+return response||fetch(event.request);
+})
+);
+
+});
